dd1031a593cfb79d04f304bedca74af5ce4edcb6
ShuchiSenpai <98480992+ShuchiSenpai@users.noreply.github.com>
2025-01-26 11:11:00 +0100

    interqctible

---
+++ Interact/interact_ray.gd
@@ -0,0 +1,15 @@
+extends RayCast3D
+
+@onready var prompt = $Prompt
+
+
+func _physics_process(_delta):
+	prompt.text = ""
+	if is_colliding():
+		var collider = get_collider()
+		
+		if collider is Interactable:
+			prompt.text = collider.prompt_message
+			
+			if Input.is_action_just_pressed("interact") :
+				collider.interact(owner)
