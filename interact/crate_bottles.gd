99d9b56bf0acc490b6c4edca2b452dd49dd368cc
Meca <mecaman01@gmail.com>
2025-01-26 13:54:43 +0100

    bug removal

--- interact/crate_bottles.gd
+++ interact/crate_bottles.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-  
+   
 
 func _on_interacted(body: Variant) -> void:  
 	print("test")
--- interact/interact_ray.gd
+++ interact/interact_ray.gd
@@ -3,7 +3,7 @@ extends RayCast3D
 @onready var prompt = $Prompt 
  
 
-func _physics_process(_delta):
+func _physics_process(_delta): 
 	prompt.text = ""
 	if is_colliding():
 		var collider = get_collider() 
